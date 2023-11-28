745e246a9ebb97f0ca2cdc4bdbed2dec9827be45
Francisco Vieira <2212520@my.ipleiria.pt>
2023-11-28 19:19:16 +0000

    Modelos
    parte inicial

---
+++ SOMIOD/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SOMIOD.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Parent { get; set; }
+        public string Event {  get; set; }
+        public string Endpoint {  get; set; }
+    }
+}
