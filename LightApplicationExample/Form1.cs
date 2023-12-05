42cffb2dc165e68d02ad0d777754ecd029cc5be4
Ricardo <rica98@sapo.pt>
2023-12-05 20:39:38 +0000

    adicionado uma aplicação exemplo de ligar e desligar lampada. Esta incluido template para cirar XML

---
+++ LightApplicationExample/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LightApplicationExample
+{
+    public partial class Form1 : Form
+    {
+        private bool lightState = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Luz";
+            if(checkBox1.Checked)
+            {
+                lightState = true;
+                label1.Text = label1.Text + " - Ligado";
+            }
+            else
+            {
+                lightState = false;
+                label1.Text = label1.Text + " - Desligado";
+            }
+
+
+            //Criar o XML template. APP, DATA
+            XmlDocument doc = new XmlDocument();
+
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
+            doc.AppendChild(dec);
+
+            XmlElement application = doc.CreateElement("Application");
+            application.SetAttribute("Name","Lighting");
+
+            XmlElement container = doc.CreateElement("Container");
+            container.SetAttribute("Name", "LightBulb");
+
+            XmlElement data = doc.CreateElement("Data");
+            data.InnerText = lightState.ToString();
+            
+            container.AppendChild(data);
+            application.AppendChild(container);
+            doc.AppendChild(application);
+            string xmlOutput = doc.OuterXml;
+
+
+            //Fazer pedido para API para criar a APlication
+            string APIPath = "http://localhost:59707/somiod";
+            HttpClient client = new HttpClient();
+            var httpContent = new StringContent(xmlOutput, Encoding.UTF8, "text/xml");
+            
+            client.PostAsync(APIPath, httpContent);
+        }
+    }
+}
--- SOMIOD/Controllers/HomeController.cs
+++ SOMIOD/Controllers/HomeController.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace SOMIOD.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
-        public ActionResult Index()
+        public IHttpActionResult Index()
         {
-            return View();
+            return Ok();
         }
-        
+
+        [Route("somiod")]
+        [HttpPost]
+        public IHttpActionResult PostData([FromBody] string data)
+        {
+            //gurdar na BD o estado
+            return Ok();
+        }
     }
 }
